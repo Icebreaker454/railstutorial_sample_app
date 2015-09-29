ad6751fe262d965f2274077d9e0241e00a99aaf8
Paul Pukach <pavlopukach@gmail.com>
2015-09-29 17:46:33 +0300

    Refactored RSpec tests; Closed #3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Sample App |" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +13,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Sample App | Home")
+      expect(page).to have_title("#{base_title} Home")
     end
 
   end
@@ -24,7 +26,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Sample App | Help")
+      expect(page).to have_title("#{base_title} Help")
     end
   end
 
@@ -36,7 +38,7 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Sample App | About")
+      expect(page).to have_title("#{base_title} About")
     end
   end
 end
